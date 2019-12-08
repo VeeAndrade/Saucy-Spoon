078ef95d61795f19b2ef42751d48c2255b15d2c7
Veronica Andrade <veronica726@gmail.com>
2019-12-08 16:06:49 -0700

    Create methods that add and remove from favorites array

--- src/user.js
+++ src/user.js
@@ -6,6 +6,15 @@ class User {
     this.favoriteRecipes = [];
     this.recipesToCook = [];
   }
+
+  addToFavorites(recipe) {
+    this.favoriteRecipes.push(recipe)
+  }
+
+  removeFromFavorites(recipe) {
+    let i = this.favoriteRecipes.forEach((r, i) => recipe.id === r.id ? i : r)
+    this.favoriteRecipes = this.favoriteRecipes.splice(i, 0)
+  }
 }
 
 if (typeof module !== 'undefined') {
